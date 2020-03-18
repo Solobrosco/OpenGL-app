13c8d08b5aad908cf15a1ceb49d0ae080e8d3b38
Nikko <nikkosolon24@gmail.com>
2020-03-17 23:05:54 -0700

    App title changed

--- src/app_main.cpp
+++ src/app_main.cpp
@@ -1,7 +1,7 @@
 #include <App.h>
 
 int main(int argc, char** argv) {
-    GlutApp* app = new App(argc, argv, 1000, 1000, "OpenGL Demo");
+    GlutApp* app = new App(argc, argv, 1000, 1000, "OpenGL Application");
 
     app->run();
 }
